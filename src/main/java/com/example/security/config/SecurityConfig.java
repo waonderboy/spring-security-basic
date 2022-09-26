63e6d68d262f18d32284c04926a3439bc3ad944f
waonderboy <crazy7523@gmail.com>
2022-09-26 10:20:25 +0900

    Basic FilterChain Config of Login

---
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 활성화 : 여기에 작성하는 스프링 시큐리티 필터가 스프링 필터체인에 등록
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login"); // 권한이 없으면 로그인 페이지로 이동
+        return http.build();
+    }
+}
--- src/main/java/com/example/security/controller/IndexController.java
+++ src/main/java/com/example/security/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -12,5 +13,36 @@ public class IndexController {
         // 뷰 리졸버 설정 prefix: /templates/ , suffix: .mustache
         // index.mustache
         return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    @ResponseBody
+    public String joinProc() {
+        return "회원가입 완료됨";
     }
 }
